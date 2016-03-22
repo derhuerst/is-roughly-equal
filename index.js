599067b0a8f7f185d9b2317eeecacd7e7c4d5f05
Jannis R <mail@jannisr.de>
2016-03-22 19:39:56 +0100

    implementation :clap:

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const notEnoughArgs = new Error('not enough arguments')
+const missingArgs = function () { throw notEnoughArgs }
+
+
+
+const unary = function (tolerance) {
+	if (arguments.length < 1) throw notEnoughArgs
+	return function (a, b) {
+		if (arguments.length < 2) throw notEnoughArgs
+		return Math.abs(b - a) <= tolerance
+	}
+}
+
+const binary = function (tolerance, a) {
+	if (arguments.length < 2) throw notEnoughArgs
+	return function (b) {
+		if (arguments.length < 1) throw notEnoughArgs
+		return Math.abs(b - a) <= tolerance
+	}
+}
+
+const ternary = function (tolerance, a, b) {
+	if (arguments.length < 3) throw notEnoughArgs
+	return Math.abs(b - a) <= tolerance
+}
+
+const arities = [missingArgs, unary, binary, ternary]
+const auto = function (tolerance, a, b) {
+	return arities[arguments.length](tolerance, a, b)
+}
+
+
+
+module.exports = Object.assign(auto, {ternary, binary, unary})
